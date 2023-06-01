c4416a7ad14a00f98e6a42b9cfd581b8e611914c
Kayanski <kowalski.kowalskin@gmail.com>
2023-06-01 11:38:28 +0200

    Added withdraw function

--- src/handlers/execute.rs
+++ src/handlers/execute.rs
@@ -1,7 +1,8 @@
 use cosmwasm_std::{Uint128, CosmosMsg};
-use abstract_sdk::Execution;
+use abstract_sdk::{Execution};
 use abstract_sdk::features::AbstractResponse;
-use cosmwasm_std::{DepsMut, Env, MessageInfo, Response, wasm_execute, Coin};
+use cosmwasm_std::{DepsMut, Env, MessageInfo, Response, wasm_execute, Coin, to_binary};
+use cw_asset::{Asset, AssetInfo};
 
 use crate::handlers::query::query_market_config;
 
@@ -19,7 +20,7 @@ pub fn execute_handler(
 ) -> AppResult {
     match msg {
         AppExecuteMsg::Deposit {amount} => execute_deposit(deps, info, app, amount),
-        AppExecuteMsg::Withdraw {amount} => execute_deposit(deps, info, app, amount),
+        AppExecuteMsg::Withdraw {amount} => execute_withdraw(deps, info, app, amount),
         AppExecuteMsg::UpdateConfig {} => update_config(deps, info, app),
     }
 }
@@ -44,6 +45,26 @@ fn execute_deposit(deps: DepsMut, msg_info: MessageInfo, app: App, amount: Uint1
     let deposit_msg = executor.execute(vec![deposit_msg.into()]);
 
     Ok(app.tag_response(Response::default(), "update_config").add_messages(deposit_msg))
+}
+
+/// Update the configuration of the app
+fn execute_withdraw(deps: DepsMut, msg_info: MessageInfo, app: App, amount: Uint128) -> AppResult {
+    // Only the admin should be able to call this, because this is a private account
+    app.admin.assert_admin(deps.as_ref(), &msg_info.sender)?;
+
+    let config = CONFIG.load(deps.storage)?;
+    let market_config = query_market_config(deps.as_ref())?;
+    // We call an action on the proxy directly
+    let executor = app.executor(deps.as_ref());
+
+    let redeem_message = Asset{
+        amount,
+        info: AssetInfo::Cw20(deps.api.addr_validate(&market_config.aterra_contract)?)
+    }.send_msg(config.market_contract, to_binary(&moneymarket::market::Cw20HookMsg::RedeemStable {  })?)?.into();
+
+    let redeem_messages = executor.execute(vec![redeem_message]);
+
+    Ok(app.tag_response(Response::default(), "update_config").add_messages(redeem_messages))
 }
 
 /// Update the configuration of the app
